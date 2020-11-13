8cf6cdfd5acc2e30e0129d9d126b2e271e34ef34
Christian Harris <harris.chrw@gmail.com>
2020-11-13 12:07:16 -0700

    Tested connection to application user on handyandydb server.

--- src/application/HandyAndyApplication.java
+++ src/application/HandyAndyApplication.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,6 +11,7 @@ import javafx.scene.Scene;
 //import org.apache.pdfbox.pdmodel.PDDocument;
 
 import menu.LoginMenu;
+import user.User;
 
 /**
  *
@@ -14,12 +19,27 @@ import menu.LoginMenu;
  */
 
 public class HandyAndyApplication extends Application{
+    
+    private User currentUser = null;
+    
+    private static final String APP_USER = "application";
+    private static final String APP_PSWD = "applicationPassword";
+    private static final String CONN_STR = "jdbc:mysql://localhost:3306/handyandydb";
 
     @Override
     public void start(Stage stage){
-        LoginMenu loginMenu = new LoginMenu();
-        Scene scene = new Scene(loginMenu, 600, 400);
-        stage.setScene(scene);
+        Connection conn = null;
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(CONN_STR, APP_USER, APP_PSWD);
+            System.out.println("Connected");
+        }
+        catch(SQLException ex){
+            System.out.println(ex);
+        }
+        //LoginMenu loginMenu = new LoginMenu();
+        //Scene scene = new Scene(loginMenu, 600, 400);
+        //stage.setScene(scene);
         stage.setTitle("HandyAndyApplication");
         stage.show();
     }
